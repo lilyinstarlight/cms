93093e137caf257e9f22d85d951eb77dfb7be142
Foster McLane <fkmclane@gmail.com>
2016-12-11 15:24:04 -0500

    reorder config

--- cms/config.py
+++ cms/config.py
@@ -5,12 +5,12 @@ addr = ('', 8080)
 log = '/var/log/cms/cms.log'
 httplog = '/var/log/cms/http.log'
 
-# root directory of markdown files
-root = '/var/www/cms'
-
 # template directory to use
 import os.path
 template = os.path.dirname(__file__) + '/html'
+
+# root directory of markdown files
+root = '/var/www/cms'
 
 # markdown extensions
 extensions = ['extra', 'codehilite', 'smarty', 'toc']
