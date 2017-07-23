87c0cc68b8bb1a4806ffd3c63545b065f0130fc3
Foster McLane <fkmclane@gmail.com>
2017-07-23 16:33:42 -0400

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python3
-from distutils.core import setup
+import os
+import re
 
-from cms import name, version
+from setuptools import setup, find_packages
+
+
+name = None
+version = None
+
+
+def find(haystack, *needles):
+    regexes = [(index, re.compile("^{}\s*=\s*'([^']*)'$".format(needle))) for index, needle in enumerate(needles)]
+    values = ['' for needle in needles]
+
+    for line in haystack:
+        if len(regexes) == 0:
+            break
+
+        for rindex, (vindex, regex) in enumerate(regexes):
+            match = regex.match(line)
+            if match:
+                values[vindex] = match.groups()[0]
+                del regexes[rindex]
+                break
+
+    return values
+
+
+with open(os.path.join(os.path.dirname(__file__), 'cms', '__init__.py'), 'r') as cms:
+    name, version = find(cms, 'name', 'version')
 
 
 setup(
@@ -11,6 +38,8 @@ setup(
     license='MIT',
     author='Foster McLane',
     author_email='fkmclane@gmail.com',
-    packages=['cms'],
-    package_data={'cms': ['html/*.*']},
+    install_requires=['markdown', 'feedgen'],
+    packages=find_packages(),
+    package_data={'': ['html/*.*', 'res/*.*']},
+    entry_points = {'console_scripts': ['cms = cms.main']},
 )
