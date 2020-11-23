ffc67b45e3eddc9d6c13913544e107fd5fab188c
Lily Foster <lily@lily.flowers>
2020-11-23 16:48:44 -0500

    set log level to info

--- cms/config.py
+++ cms/config.py
@@ -58,6 +58,8 @@ def _apply():
     else:
         _logging.getLogger('cms').addHandler(_logging.StreamHandler(_sys.stdout))
 
+    _logging.getLogger('cms').setLevel(_logging.INFO)
+
     if http_log:
         http_log_handler = _logging.FileHandler(http_log)
         http_log_handler.setFormatter(_web.HTTPLogFormatter())
